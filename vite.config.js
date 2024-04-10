efdc3d8f2415a8e15b73dcc5a3cd1804c7a7a49f
swapneel1026 <swapneelshubham44@gmail.com>
2024-04-10 18:02:36 +0530

    fix:proxy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
+import { createProxy } from "vite-plugin-proxy";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 const HOSTNAME = "https://docter-app-backend.onrender.com";
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createProxy({
+      "/api": {
+        target: "https://docter-app-backend.onrender.com",
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
