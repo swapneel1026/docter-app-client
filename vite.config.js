4b91bebe2add7ab64f64f8a02de020d964249da1
swapneel1026 <swapneelshubham44@gmail.com>
2024-04-10 17:55:25 +0530

    vite config proxy

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000" || HOSTNAME,
+        target: HOSTNAME || "http://localhost:8000",
         changeOrigin: true,
         secure: false,
       },
