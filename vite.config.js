6e3ec54e5d2cdad5b36aaddaf796c61cad8c9d38
swapneel1026 <swapneelshubham44@gmail.com>
2024-04-10 17:46:12 +0530

    fix:vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-const HOSTNAME = "https://doctor-appointment-system-3-borg.onrender.com";
+const HOSTNAME = "https://docter-app-backend.onrender.com";
 export default defineConfig({
   plugins: [react()],
   server: {
