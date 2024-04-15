60df5fdb85205dc820e8133353bdbbd6311dd5e6
swapneel1026 <swapneelshubham44@gmail.com>
2024-04-16 03:13:33 +0530

    fix url to https

--- src/compo/PdfRenderer.jsx
+++ src/compo/PdfRenderer.jsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 
 const PdfViewer = ({ url }) => {
+  const modifiedUrl = url.replace("http://", "https://");
   return (
     <div>
       <embed
-        src={url}
+        src={modifiedUrl}
         type="application/pdf"
         style={{
           width: "100%",
